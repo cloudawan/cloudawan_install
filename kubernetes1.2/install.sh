7632911d4c9b1403446651a8afcb1f3e197b3649
edcyc <edcyc.tw@gmail.com>
2016-07-09 10:05:44 +0800

    Add script

---
+++ kubernetes1.2/install.sh
@@ -0,0 +1,11 @@
+#Install Ansible
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
+sudo apt-get install -y --force-yes ansible
+
+
+# To avoid the known host list issue
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+ansible-playbook -i hosts --timeout=1800 -vvvv site.yml
